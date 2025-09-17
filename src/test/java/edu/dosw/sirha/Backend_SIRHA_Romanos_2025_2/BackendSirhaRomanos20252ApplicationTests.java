d90abc1f1d1f51b1a2fa3b64cbb9a7cdd03f4cfb
unknown <juan.ortega-m@labinfo.is.escuelaing.edu.co>
2025-09-17 12:16:09 -0500

    21-style: Configurar proyecto y pom inicial

---
+++ src/test/java/edu/dosw/sirha/Backend_SIRHA_Romanos_2025_2/BackendSirhaRomanos20252ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.dosw.sirha.Backend_SIRHA_Romanos_2025_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSirhaRomanos20252ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
