8c1d7e3335b3fe58d6d5ff06712628d3387d7460
Juanseom <juanse.om13@gmail.com>
2025-09-24 13:37:15 -0500

    195-feat: Implementar documentacion para clases en services/observer

--- src/main/java/edu/dosw/sirha/services/observer/Alert.java
+++ src/main/java/edu/dosw/sirha/services/observer/Alert.java
@@ -1,5 +1,9 @@
 package edu.dosw.sirha.services.observer;
 
+/**
+ * Interface representing an alert in the system.
+ * Alerts can be of various types and levels, and can be sent to notify users or systems.
+ */
 public interface Alert {
     String getMessage();
     String getType();
