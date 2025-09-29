7aaf65169626617bca5c8efb46c7e263a5c7fb5b
Elizabeth Correa <elizabeth.correa-s@mail.escuelaing.edu.co>
2025-09-29 17:48:03 -0500

    222 feat: implementar entities: AcademicVicePresident, ClassSchedule, classSession

--- src/main/java/edu/dosw/sirha/model/entities/AcademicVicePresident.java
+++ src/main/java/edu/dosw/sirha/model/entities/AcademicVicePresident.java
@@ -1,4 +1,24 @@
 package edu.dosw.sirha.model.entities;
 
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.Setter;
+import lombok.Getter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Document(collection = "academicVicePresidents")
+
 public class AcademicVicePresident extends User{
+
+    public AcademicVicePresident(String name, String mail, String document) {
+        super(name, mail, document, UserType.ACADEMIC_VICEPRESIDENT);
+    }
 }
