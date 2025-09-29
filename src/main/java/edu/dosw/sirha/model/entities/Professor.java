8fe4ab6fe708581e57ef56607b53c0e3acad5065
Juan Pablo Contreras <juan.contreras-p@mail.escuelaing.edu.co>
2025-09-29 17:45:31 -0500

    222 feat: Implementar entities: Petition, Professor n Schedule.

--- src/main/java/edu/dosw/sirha/model/entities/Professor.java
+++ src/main/java/edu/dosw/sirha/model/entities/Professor.java
@@ -1,4 +1,25 @@
 package edu.dosw.sirha.model.entities;
 
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.Setter;
+import lombok.Getter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Document(collection = "professors")
+
+/**
+ * Represents a professor of the institution
+ */
 public class Professor extends User{
+    public Professor(String name, String mail, String document) {
+        super(name, mail, document, UserType.PROFESSOR);
+    }
 }
